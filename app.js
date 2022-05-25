82750fb6a1d0f3903554f0d4c193968c66376f2c
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-05-25 14:53:10 +0300

    Add transition to game screen

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
+const game = document.querySelector('.game');
 const startScreen = document.querySelector('.start-screen');
-const start = document.querySelector('.btn-start');
+const gameScreen = document.querySelector('.game-screen');
+const startBtn = document.querySelector('.btn-start');
 const pveBtn = document.querySelector('.btn-pve');
 const pvpBtn = document.querySelector('.btn-pvp');
 const gameMode = document.querySelector('.game-mode-buttons');
 const players = document.querySelector('.players');
+const submitBtn = document.querySelector('.btn-submit');
 
-start.addEventListener('click', (e) => handleTransition(e, start, gameMode));
+startBtn.addEventListener('click', (e) =>
+  handleTransition(e, startBtn, gameMode)
+);
 pvpBtn.addEventListener('click', (e) => handleTransition(e, gameMode, players));
+submitBtn.addEventListener('click', handleStartGame);
+
+function handleStartGame(e) {
+  e.preventDefault();
+
+  handleTransition(e, startScreen, gameScreen);
+}
 
 function handleTransition(e, fadeOutElement, fadeInElement) {
   fadeOutElement.style.opacity = '0';
-  fadeOutElement.style.transition = 'all 0.4s ease-in-out';
-  fadeInElement.style.display = 'flex';
-
   fadeOutElement.addEventListener('transitionend', handleTransitionEnd);
 
   function handleTransitionEnd(e) {
     fadeOutElement.removeEventListener('transitionend', handleTransitionEnd);
+    fadeInElement.style.display = 'flex';
     fadeOutElement.style.display = 'none';
     fadeInElement.style.opacity = '1';
   }

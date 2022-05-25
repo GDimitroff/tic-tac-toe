41674f435bf8a119f269f3d512f4d0cd3d1235e6
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-05-25 12:33:52 +0300

    Add generic transition function

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const startScreen = document.querySelector('.start-screen');
-const startBtn = document.querySelector('.btn-start');
+const start = document.querySelector('.btn-start');
 const pveBtn = document.querySelector('.btn-pve');
 const pvpBtn = document.querySelector('.btn-pvp');
 const gameMode = document.querySelector('.game-mode-buttons');
+const players = document.querySelector('.players');
 
-startBtn.addEventListener('click', handleStartGame);
+start.addEventListener('click', (e) => handleTransition(e, start, gameMode));
+pvpBtn.addEventListener('click', (e) => handleTransition(e, gameMode, players));
 
-function handleStartGame(e) {
-  startBtn.style.opacity = '0';
-  startBtn.style.transition = 'all 0.4s ease-in-out';
-  gameMode.style.display = 'flex';
+function handleTransition(e, fadeOutElement, fadeInElement) {
+  fadeOutElement.style.opacity = '0';
+  fadeOutElement.style.transition = 'all 0.4s ease-in-out';
+  fadeInElement.style.display = 'flex';
 
-  startBtn.addEventListener('transitionend', handleTransitionEnd);
+  fadeOutElement.addEventListener('transitionend', handleTransitionEnd);
 
   function handleTransitionEnd(e) {
-    startScreen.removeEventListener('transitionend', handleTransitionEnd);
-    startBtn.style.display = 'none';
-    gameMode.style.opacity = '1';
+    fadeOutElement.removeEventListener('transitionend', handleTransitionEnd);
+    fadeOutElement.style.display = 'none';
+    fadeInElement.style.opacity = '1';
   }
 }
